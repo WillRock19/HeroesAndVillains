aa8cde47d35838fb697135a6ff066100347d78c7
William Porto <william@nfe.io>
2023-05-09 10:55:59 -0300

    Adding base class for accessing and using MongoDb database

---
+++ src/HeroesAndVillains.Infrastructure/Repositories/MongoDbRepository.cs
@@ -0,0 +1,40 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace HeroesAndVillains.Infrastructure.Repositories
+{
+    public abstract class MongoDbRepository<T>
+    {
+        protected readonly IMongoCollection<T> Collection;
+
+        public MongoDbRepository(string connectionString, string databaseName, string collectionName)
+        {
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase(databaseName);
+            Collection = database.GetCollection<T>(collectionName);
+        }
+
+        public async Task<T> Get(ObjectId id) 
+        {
+            var filterById = Builders<T>.Filter.Eq("_id", id);
+            var result = await Collection.FindAsync(filterById);
+            return await result.FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<T>> GetAll(ObjectId id)
+        {
+            var result = await Collection.FindAsync(new BsonDocument());
+            return await result.ToListAsync();
+        }
+
+        public async Task Save(T item) 
+        {
+            await Collection.InsertOneAsync(item);
+        }
+
+        public async Task SaveAll(IEnumerable<T> item)
+        {
+            await Collection.InsertManyAsync(item);
+        }
+    }
+}
