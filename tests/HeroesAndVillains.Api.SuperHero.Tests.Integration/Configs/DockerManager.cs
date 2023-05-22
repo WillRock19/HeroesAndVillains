6f150124f0f3bc90e8f3b83b2944fac65cd39baa
WillRock19 <williamporto_14@hotmail.com>
2023-05-22 13:50:51 -0300

    Adding mongoDb and first integration tests to the project  (#2)
    * Adding base class for accessing MongoDb database;
    
    * Adding MongoDbRepository and Integration test;
    
    * Implementing tests for MongoDb and also tests for AzureBlob;
    
    ---------
    
    Co-authored-by: William Porto <william@nfe.io>

---
+++ tests/HeroesAndVillains.Api.SuperHero.Tests.Integration/Configs/DockerManager.cs
@@ -0,0 +1,30 @@
+﻿using Docker.DotNet;
+using Docker.DotNet.Models;
+
+namespace HeroesAndVillains.Api.SuperHero.Tests.Integration.Configs
+{
+    internal class DockerManager : IDisposable
+    {
+        private const string _runningState = "running";
+        private readonly DockerClient _dockerClient;
+
+        public DockerManager() 
+        {
+            _dockerClient = new DockerClientConfiguration().CreateClient();
+        }
+
+        public async Task<bool> ContainersWithDatabasesAreUp(IEnumerable<string> containerNames) 
+        {
+            var containers = await _dockerClient.Containers.ListContainersAsync(
+                new ContainersListParameters { All = true }
+            );
+
+            return containerNames.All(name => containers.Any(x => x.Names.First().Contains(name) && x.State == _runningState));
+        }
+
+        public void Dispose() 
+        {
+            _dockerClient.Dispose();
+        }
+    }
+}
---
+++ tests/HeroesAndVillains.Api.SuperHero.Tests.Integration/Configs/GlobalContext.cs
@@ -0,0 +1,39 @@
+﻿namespace HeroesAndVillains.Api.SuperHero.Tests.Integration.Configs
+{
+    public class GlobalContext : IDisposable
+    {
+        public string MongoDbConnectionString => "mongodb://localhost:27017";
+        public string AzuriteConnectionString => "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;";
+
+        private DockerManager _dockerManager;
+
+        public GlobalContext()
+        {
+            _dockerManager = new DockerManager();
+
+            var containerNames = new List<string>()
+            {
+                "mongo-db-for-tests",
+                "azurite-for-tests"
+            };
+            EnsureContainersAreUp(containerNames);
+        }
+
+        public void Dispose() 
+        {
+            _dockerManager.Dispose();
+        }
+
+        private void EnsureContainersAreUp(List<string> containerNames) 
+        {
+            var allRunning = _dockerManager
+                .ContainersWithDatabasesAreUp(containerNames)
+                .Result;
+
+            if (!allRunning)
+            {
+                throw new ApplicationException("Not all obligatory containers are running; tests cannot be executed!");
+            }
+        }
+    }
+}
