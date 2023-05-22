78191bcf1012ee3c06b505188dc6d290a1a858d9
WillRock19 <williamporto_14@hotmail.com>
2023-05-22 18:50:09 -0300

    Starting containers from a process inside the Test's SetUp (#3)
    * Running containers from inside the dotnet code
    ---------
    
    Co-authored-by: William Porto <william@nfe.io>

--- tests/HeroesAndVillains.Api.SuperHero.Tests.Integration/Configs/GlobalContext.cs
+++ tests/HeroesAndVillains.Api.SuperHero.Tests.Integration/Configs/GlobalContext.cs
@@ -1,34 +1,40 @@
-﻿namespace HeroesAndVillains.Api.SuperHero.Tests.Integration.Configs
-{
-    public class GlobalContext : IDisposable
-    {
-        public string MongoDbConnectionString => "mongodb://localhost:27017";
-        public string AzuriteConnectionString => "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;";
+﻿using HeroesAndVillains.Tests.Common.Helpers;
 
-        private DockerManager _dockerManager;
+namespace HeroesAndVillains.Api.SuperHero.Tests.Integration.Configs
+{
+    public class GlobalContext : IAsyncLifetime
+    {
+        public string MongoDbConnectionString => "mongodb://localhost:27017";    
+        public string AzuriteConnectionString => "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;";
+        
+        private DockerComposeManager _dockerManager;
 
         public GlobalContext()
         {
-            _dockerManager = new DockerManager();
-
             var containerNames = new List<string>()
             {
                 "mongo-db-for-tests",
                 "azurite-for-tests"
             };
-            EnsureContainersAreUp(containerNames);
+            var dockerComposePath = Path.Combine(Directory.GetCurrentDirectory(), "docker-compose.yml");
+
+            _dockerManager = new DockerComposeManager(containerNames, dockerComposePath);
         }
 
-        public void Dispose() 
+        public async Task InitializeAsync()
         {
+            await EnsureContainersAreUp();
+        }
+
+        async Task IAsyncLifetime.DisposeAsync()
+        {
+            await _dockerManager.StopAndRemoveContainers();
             _dockerManager.Dispose();
         }
 
-        private void EnsureContainersAreUp(List<string> containerNames) 
+        private async Task EnsureContainersAreUp() 
         {
-            var allRunning = _dockerManager
-                .ContainersWithDatabasesAreUp(containerNames)
-                .Result;
+            var allRunning = await _dockerManager.GuaranteeContainersAreUp();
 
             if (!allRunning)
             {
